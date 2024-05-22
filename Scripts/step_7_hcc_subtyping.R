a2d501acd63b24cdc3dfb37fd477f8433d8191d7
Kui Chen <109993615+chnqwe7@users.noreply.github.com>
2024-05-22 09:35:25 -0400

    Update step_7_hcc_subtyping.R

--- Scripts/step_7_hcc_subtyping.R
+++ Scripts/step_7_hcc_subtyping.R
@@ -1,24 +1,20 @@
-# Description: This script was employed to analysis HCC subtypes on different chronic liver disease backgrounds using “one-vs-each” tumor subtyping machine learning strategy.
+# Description: This script was employed to analysis HCC subtypes based on the different liver disease backgrounds using “one-vs-each” tumor subtyping machine learning strategy.
 ## Author：Kui Chen → kui.chen@uhn.ca, Ping Luo → ping.luo@uhn.ca ###################################
 ## Date Created: 2022-09-01 #########################################################################
 ## Last Modified: 2023-12-10 ########################################################################
-# Runnning ~ 2 days at H4H cluster with ≥ 500GB memory (superhimem) configuration ###################
-# Running well with R/4.0.1 module at H4H ###########################################################
 # Before starting, do the followings ################################################################
 # 1.make working directory "/path/to/your/counts" ###################################################
-# 2.using source() to introduce the OnevsEach_hcc.R code ############################################
-# 3.require "n236_cpm.rds", "n236_lcpm.rds","sample_113.txt", "sample_124.txt" ######################
+# 2.using source() to introduce the step_7s_one_vs_each_hcc.R code ##################################
+# 3.require "n236_lcpm.rds", "sample_124.rds" #######################################################
 
 ## Abbreviations ####################################################################################
 # ALD, alcoholic liver disease ######################################################################
 # CTL, healthy control ##############################################################################
 # HBV, hepatitis B virus ############################################################################
 # HCV, hepatitis C virus ############################################################################
-# NASH, non-alcoholic steatohepatitis ###############################################################
+# MASH, Metabolic dysfunction-associated steatohepatitis ############################################
 # ND, no known liver disease ########################################################################
 
-rm(list=ls())
-gc()
 library(dplyr)
 library(ggplot2)
 library(reshape2)
@@ -36,25 +32,27 @@ setwd("/path/to/your/counts")
 # Prepare matrix
 load("n236_lcpm.RData")
 
-count <- readRDS("n236_lcpm.rds")
-count <- as.data.frame(count)
-phenotype <- read.table(file="sample_124.txt", sep = "\t", header=T, stringsAsFactors=FALSE)
-table(phenotype$Group)
+#Prepare matrix
+count <- readRDS('n236_lcpm.rds')
+phenotype <- readRDS('sample_124.rds')
+
+table(phenotype$Disease)
+table(phenotype$Cirr_group)
+
 matrix <- count[ ,match(phenotype$Sample_ID,colnames(count))]
 identical(colnames(matrix), phenotype$Sample_ID)
 
 #check how many DMR in black_list
 load("black_bin_v2.RData")
-head(black_bin$black_bin)
 matrix <- subset(matrix, !rownames(matrix) %in% black_bin$black_bin)
 identical(colnames(matrix), phenotype$Sample_ID)
 
-source("OnevsEach_hcc.R")
+source("step_7s_one_vs_each_hcc.R")
 
 Features.CVparam <- trainControl(method = "repeatedcv",number = 10, repeats =3, verboseIter=TRUE,returnData=FALSE, classProbs = TRUE,savePredictions=FALSE)
 
 # Define the subclasses
-subclasses <- c("HBV", "HCV", "ALD", "NASH", "ND", "CTL")
+subclasses <- c("HBV", "HCV", "ALD", "MASH", "ND", "CTL")
 
 # Create lists to store the results
 All.kFold <- list()
@@ -67,7 +65,7 @@ for (subclass in subclasses) {
  
  for (j in 1:100) {
   # Split the data for 100 times, result of each split is saved in Splits10
-  Splits10[[j]] <- SplitkFold(matrix, phenotype$Group, 10)
+  Splits10[[j]] <- SplitkFold(matrix, phenotype$Disease, 10)
   
   # 10-fold cross validation to calculate the probability (score) of each sample being the subclass
   kFold.list <- list()
@@ -172,25 +170,25 @@ roc_ald_smooth <- smooth(roc_ald, method = "density")
 roc_ald <- data.frame(Sens = roc_ald_smooth$sensitivities,  Spec = roc_ald_smooth$specificities, stringsAsFactors = F)
 
 
-# NASH Subclass
-load("hcc_NASH_res.RData")
-nash_predictions <- lapply(TestPerformance.list, function(x) x%>%mutate(Class2 = ifelse(ActualClass == "NASH",1,0))%>%mutate(Class2 = as.numeric(Class2)))
-for(i in 1:length(nash_predictions)) { nash_predictions[[i]]$Index <- paste0("Model_",i) }
-ROCs.computed <- lapply(nash_predictions, function(x) with(x, roc(Class2 ~ One)))
+# MASH Subclass
+load("hcc_MASH_res.RData")
+MASH_predictions <- lapply(TestPerformance.list, function(x) x%>%mutate(Class2 = ifelse(ActualClass == "MASH",1,0))%>%mutate(Class2 = as.numeric(Class2)))
+for(i in 1:length(MASH_predictions)) { MASH_predictions[[i]]$Index <- paste0("Model_",i) }
+ROCs.computed <- lapply(MASH_predictions, function(x) with(x, roc(Class2 ~ One)))
 ROCs.computed <- lapply(ROCs.computed, function(x) smooth(x, method = "density"))
 ROCs.computed <- lapply(ROCs.computed, function(x) data.frame(Sens = x$sensitivities, Spec = x$specificities, stringsAsFactors = F))
-AUCs <- lapply(nash_predictions, function(x) with(x, roc(Class2 ~ One)$auc))
+AUCs <- lapply(MASH_predictions, function(x) with(x, roc(Class2 ~ One)$auc))
 AUCs <- unlist(AUCs)
 # Obtain AUROC
 t.test(AUCs, conf.level = 0.95)
 # Combine all model predictions into a large data frame
-combined_predictions <- do.call(rbind, nash_predictions)
+combined_predictions <- do.call(rbind, MASH_predictions)
 # Compute the overall ROC
-roc_nash <- with(combined_predictions, roc(Class2 ~ One))
+roc_MASH <- with(combined_predictions, roc(Class2 ~ One))
 # Compute the smoothed ROC
-roc_nash_smooth <- smooth(roc_nash, method = "density")
+roc_MASH_smooth <- smooth(roc_MASH, method = "density")
 # Extract the sensitivities and specificities
-roc_nash <- data.frame(Sens = roc_nash_smooth$sensitivities,  Spec = roc_nash_smooth$specificities,  stringsAsFactors = F)
+roc_MASH <- data.frame(Sens = roc_MASH_smooth$sensitivities,  Spec = roc_MASH_smooth$specificities,  stringsAsFactors = F)
 
 
 # HCV Subclass
@@ -236,17 +234,17 @@ roc_nd <- data.frame(Sens = roc_nd_smooth$sensitivities, Spec = roc_nd_smooth$sp
 # combine all ROC curves
 roc_hbv$Dataset <- "HBV"
 roc_ald$Dataset <- "ALD"
-roc_nash$Dataset <- "NASH"
+roc_MASH$Dataset <- "MASH"
 roc_hcv$Dataset <- "HCV"
 roc_nd$Dataset <- "ND"
 roc_ctl$Dataset <- "CTL"
 
-roc_all <- rbind(roc_hbv, roc_ald, roc_nash, roc_hcv, roc_nd, roc_ctl)
+roc_all <- rbind(roc_hbv, roc_ald, roc_MASH, roc_hcv, roc_nd, roc_ctl)
 
 ggplot(data = roc_all, aes(x = 1 - Spec, y = Sens, colour = Dataset)) +
  theme_bw() +
  geom_line(size = 5) +
- scale_color_manual(values = c("HBV" = "#d8222b", "ALD" = "#00aacb", "HCV" = "#fa9d48", "NASH" = "#424242", "ND" = "grey", "CTL" = "#5d6a85")) +
+ scale_color_manual(values = c("HBV" = "#d8222b", "ALD" = "#00aacb", "HCV" = "#fa9d48", "MASH" = "#424242", "ND" = "grey", "CTL" = "#5d6a85")) +
  theme(legend.position = "none",
        panel.border = element_rect(linewidth = 4, color = "black"),
        panel.grid.major = element_blank(), 
@@ -274,7 +272,6 @@ library(tidyr)
 
 # load lcpm matrix
 count <- readRDS("n236_lcpm.rds")
-count <- as.data.frame(count)
 
 # Extract HBV DMR
 load("hcc_HBV_res.RData")
@@ -324,8 +321,8 @@ df_temp <- re[re$Freq >= nFreq,]
 ALD_DMR <- as.data.frame(df_temp$Features_vec)
 dim(ALD_DMR)
 
-# Extract NASH DMR
-load("hcc_NASH_res.RData")
+# Extract MASH DMR
+load("hcc_MASH_res.RData")
 ## Select feature with threshold ############################
 nFreq <- 1
 Features_vec <- c()
@@ -337,8 +334,8 @@ for (j in 1:100) {
 }
 re <- as.data.frame(table(Features_vec))
 df_temp <- re[re$Freq >= nFreq,]
-NASH_DMR <- as.data.frame(df_temp$Features_vec)
-dim(NASH_DMR)
+MASH_DMR <- as.data.frame(df_temp$Features_vec)
+dim(MASH_DMR)
 
 # Extract ND (non-liver disease) DMR
 load("hcc_ND_res.RData")
@@ -373,59 +370,51 @@ df_temp <- re[re$Freq >= nFreq,]
 CTL_DMR <- as.data.frame(df_temp$Features_vec)
 dim(CTL_DMR)
 
-Features <- rbind(HBV_DMR,HCV_DMR,ALD_DMR,NASH_DMR,ND_DMR,CTL_DMR)
+Features <- rbind(HBV_DMR,HCV_DMR,ALD_DMR,MASH_DMR,ND_DMR,CTL_DMR)
 
-# check if there is any overlaps
-duplicates <- duplicated(Features)
-if (any(duplicates)) {
- print("There are duplicates.")
-} else {
- print("There are no duplicates.")
-}
+# Identify and remove all duplicated rows, including the first occurrence
+all_duplicates <- duplicated(Features) | duplicated(Features, fromLast = TRUE)
+# Print the number of duplicate rows to be removed
+num_all_duplicates <- sum(all_duplicates)
+print(paste("Number of rows to be removed (including the first occurrence):", num_all_duplicates))
 
-num_duplicates <- sum(duplicated(Features))
-print(paste("Number of duplicate rows:", num_duplicates))
-
+# Remove all duplicated rows
+Features_no_duplicates <- Features[!all_duplicates, ]
+Features_no_duplicates <- as.data.frame(Features_no_duplicates)
+# Print the number of rows before and after removing duplicates
+print(paste("Number of rows before removing duplicates:", nrow(Features)))
+print(paste("Number of rows after removing duplicates:", nrow(Features_no_duplicates)))
 
 ### PCA analysis
-colnames(Features) <- c("DMR")
-DMR_count <- count[rownames(count) %in% Features$DMR,]
-sample <- read.table(file="sample_124.txt", sep = "\t", header=T, stringsAsFactors=FALSE)
+colnames(Features_no_duplicates) <- c("DMR")
+DMR_count <- count[rownames(count) %in% Features_no_duplicates$DMR,]
+sample <- readRDS('sample_124.rds')
 pheno <- sample
-head(pheno)
-head(DMR_count)
-
-table(pheno$Group)
-table(pheno$Group_2)
+table(pheno$Disease)
+table(pheno$Cirr_group)
 DMR_count <- DMR_count[,match(pheno$Sample_ID, colnames(DMR_count))]
 identical(colnames(DMR_count), pheno$Sample_ID)
 
 dmr <- as.data.frame(DMR_count)
 dmr$bin <- rownames(dmr)
-head(dmr)
 dmr <- dmr[,c(125,1:124)]
-colnames(dmr)
 data <- as.data.frame(t(dmr[,c(2:125)]))
 data$sample <- rownames(data)
-head(data)
-data <- cbind(pheno, data[ ,c(25738,1:25737)])
+data <- cbind(pheno, data[ ,c(25643,1:25642)])
 identical(data$Sample_ID, data$sample)
-dim(data)
-data[1:10,1:12]
-res.pca <- PCA(data[ ,c(5:25741)], graph = FALSE)
-head(get_eigenvalue(res.pca))
-get_eigenvalue(res.pca)
+res.pca <- PCA(data[ ,c(9:25650)], graph = FALSE)
 
 # PCA plot
 basic_plot <- fviz_pca_ind(res.pca, label="none")
-head(basic_plot$data)
 b_d <- basic_plot$data
-table(data$Group_2)
-table(data$Group)
-b_d <- cbind(b_d, data$Group, data$Group_2,data$Sample_ID)
-head(b_d)
+b_d <- cbind(b_d, data$Disease, data$Cirr_group,data$Sample_ID)
 colnames(b_d)[c(7,8,9)] <- c("Group","Group_2","Sample")
 b_d <- b_d[c(7,8,9,2,3)]
+
+# show PC-1
+print(get_eigenvalue(res.pca)[1,2])
+# show PC-2
+print(get_eigenvalue(res.pca)[2,2])
 
 # PCA plot
 qplot(data = b_d, x = x, y = y,  size = I(0), alpha = I(0.1)) +
@@ -442,7 +431,7 @@ qplot(data = b_d, x = x, y = y,  size = I(0), alpha = I(0.1)) +
  geom_point(data = b_d %>% filter(Group == "ND" & Group_2 == "NC"), aes(y = y, x = x), shape = 21, size = 16, alpha = I(0.8), color = "black", fill = I("#c0c0c0"), stroke = 1) +
  geom_point(data = b_d %>% filter(Group == "ND" & Group_2 == "Cirrhosis"), aes(y = y, x = x), shape = 21, size = 16, alpha = I(0.8), color = "black", fill = I("#c0c0c0"), stroke = 1) +
  geom_point(data = b_d %>% filter(Group == "ND" & Group_2 == "Cirrhosis"), aes(y = y, x = x), shape = 21, size = 4, alpha = I(1), color = "black", fill = I("black"), stroke = 1.5) +
- geom_point(data = b_d %>% filter(Group == "NASH"), aes(y = y, x = x), shape = 21, size = 15, alpha = I(0.9), color = "black", fill = I("#545454"), stroke = 1) +
+ geom_point(data = b_d %>% filter(Group == "MASH"), aes(y = y, x = x), shape = 21, size = 15, alpha = I(0.9), color = "black", fill = I("#545454"), stroke = 1) +
  geom_point(data = b_d %>% filter(Group == "ALD" & Group_2 == "NC"), aes(y = y, x = x), shape = 21, size = 16, alpha = I(0.9), color = "black", fill = I("#00aacb"), stroke = 1) +
  geom_point(data = b_d %>% filter(Group == "ALD" & Group_2 == "Cirrhosis"), aes(y = y, x = x), shape = 21, size = 16, alpha = I(0.9), color = "black", fill = I("#00aacb"), stroke = 1) +
  geom_point(data = b_d %>% filter(Group == "ALD" & Group_2 == "Cirrhosis"), aes(y = y, x = x), shape = 21, size = 4, alpha = I(1), color = "black", fill = I("black"), stroke = 1.5) +
@@ -453,16 +442,16 @@ qplot(data = b_d, x = x, y = y,  size = I(0), alpha = I(0.1)) +
  geom_point(data = b_d %>% filter(Group == "Autoimmune" & Group_2 == "NC"), aes(y = y, x = x), shape = 5, size = 12, alpha = I(1), color = "blue", fill = I("#942193"), stroke = 2) +
  geom_point(data = b_d %>% filter(Group == "Autoimmune" & Group_2 == "Cirrhosis"), aes(y = y, x = x), shape = 9, size = 12, alpha = I(1), color = "blue", fill = I("#942193"), stroke = 2) +
  geom_point(data = b_d %>% filter(Group == "PBC"), aes(y = y, x = x), shape = 14, size = 12, alpha = I(1), color = "#942193", fill = I("#0051ca"), stroke = 2) +
- geom_point(data = b_d %>% filter(Group == "HBV-NASH" & Group_2 == "NC"), aes(y = y, x = x), shape = 24, size = 12, alpha = I(0.6), color = "black", fill = I("#963b40"), stroke = 1) +
- geom_point(data = b_d %>% filter(Group == "HBV-NASH" & Group_2 == "Cirrhosis"), aes(y = y, x = x), shape = 24, size = 12, alpha = I(0.6), color = "black", fill = I("#963b40"), stroke = 1) +
- geom_point(data = b_d %>% filter(Group == "HBV-NASH" & Group_2 == "Cirrhosis"), aes(y = y, x = x), shape = 21, size = 4, alpha = I(1), color = "black", fill = I("black"), stroke = 1.5) +
+ geom_point(data = b_d %>% filter(Group == "HBV-MASH" & Group_2 == "NC"), aes(y = y, x = x), shape = 24, size = 12, alpha = I(0.6), color = "black", fill = I("#963b40"), stroke = 1) +
+ geom_point(data = b_d %>% filter(Group == "HBV-MASH" & Group_2 == "Cirrhosis"), aes(y = y, x = x), shape = 24, size = 12, alpha = I(0.6), color = "black", fill = I("#963b40"), stroke = 1) +
+ geom_point(data = b_d %>% filter(Group == "HBV-MASH" & Group_2 == "Cirrhosis"), aes(y = y, x = x), shape = 21, size = 4, alpha = I(1), color = "black", fill = I("black"), stroke = 1.5) +
  geom_point(data = b_d %>% filter(Group == "HBV-ALD"), aes(y = y, x = x), shape = 22, size = 14, alpha = I(0.6), color = "black", fill = I("#942193"), stroke = 2) +
  geom_point(data = b_d %>% filter(Group == "HBV-ALD"), aes(y = y, x = x), shape = 21, size = 4, alpha = I(1), color = "black", fill = I("black"), stroke = 1.5) +
  geom_point(data = b_d %>% filter(Group == "HCV-ALD"), aes(y = y, x = x), shape = 23, size = 14, alpha = I(0.7), color = "black", fill = I("grey"), stroke = 1) +
  geom_point(data = b_d %>% filter(Group == "HCV-ALD"), aes(y = y, x = x), shape = 21, size = 4, alpha = I(1), color = "black", fill = I("black"), stroke = 1.5) +
- geom_point(data = b_d %>% filter(Group == "NASH-ALD"), aes(y = y, x = x), shape = 25, size = 12, alpha = I(0.4), color = "black", fill = I("#10a1a1"), stroke = 1) +
- geom_point(data = b_d %>% filter(Group == "NASH-ALD"), aes(y = y, x = x), shape = 6, size = 12, alpha = I(0.4), color = "black", fill = I("black"), stroke = 1) +
- xlab("PC1 (7.89%)")+ ylab("PC2 (5.31%)")+
+ geom_point(data = b_d %>% filter(Group == "MASH-ALD"), aes(y = y, x = x), shape = 25, size = 12, alpha = I(0.4), color = "black", fill = I("#10a1a1"), stroke = 1) +
+ geom_point(data = b_d %>% filter(Group == "MASH-ALD"), aes(y = y, x = x), shape = 6, size = 12, alpha = I(0.4), color = "black", fill = I("black"), stroke = 1) +
+ xlab("PC1 (7.80%)")+ ylab("PC2 (5.26%)")+
  theme(plot.margin = unit(c(0.3, 2.5, 0.5, 0.3), "cm")) +
  theme(axis.text.x = element_text(size = 55, color = "black"), axis.text.y = element_text(size = 55, color = "black"),
        axis.title.x = element_text(vjust = 0, size = 55, color = "black"), axis.title.y = element_text(vjust = -1.5, size = 55, color = "black"))
