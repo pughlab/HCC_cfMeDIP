e90bf350b13db3fdc112192a4c2ef87c7d1cbac2
Kui Chen <109993615+chnqwe7@users.noreply.github.com>
2024-05-23 11:27:14 -0400

    Update step_6_hcc_classifiy_hms.R

--- Scripts/step_6_hcc_classifiy_hms.R
+++ Scripts/step_6_hcc_classifiy_hms.R
@@ -2,9 +2,7 @@
 ## Author：Kui Chen → kui.chen@uhn.ca; Ping Luo → ping.luo@uhn.ca
 ## Date Created: 2022-07-18
 ## Last Modified: 2024-02-10
-# Before starting, do the followings
-# 1.make working directory "/path/to/your/ML"
-# 2.require support files  "n236_lcpm.rds", "sample.rds", "sample_sq.rds", "black_bin_v2.RData"
+# require support files  "n236_lcpm.rds", "sample.rds", "sample_sq.rds", "black_bin_v2.RData"
 
 library(dplyr)
 library(ggplot2)
